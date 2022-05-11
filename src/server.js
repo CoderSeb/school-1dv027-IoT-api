3af8c18478646cf55815dbfd6602b59475c7f04f
Sebastian <sa224ny@student.lnu.se>
2022-05-11 11:14:37 +0200

    Add: router.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
+import logger from 'morgan'
+import router from './routers/router.js'
 
 const main = async () => {
   const server = express()
   const port = process.env.PORT || 5000
-  server.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+
+  server.use(logger('dev'))
+  server.use('/', router)
 
   server.listen(port, (req, res) => {
     console.log(`Server is running @ http://localhost:${port}`)
