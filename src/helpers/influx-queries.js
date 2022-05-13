c163b319b47114d6d40cdf5d37fe9bd03df22840
Sebastian <sa224ny@student.lnu.se>
2022-05-13 16:11:47 +0200

    Add: interval to change flux query

--- src/helpers/influx-queries.js
+++ src/helpers/influx-queries.js
@@ -5,16 +5,17 @@ dotenv.config()
 const token = process.env.INFLUXDB_TOKEN
 const url = process.env.INFLUXDB_URL
 const org = process.env.INFLUXDB_ORG
-export const queryByField = async (field, since) => {
+export const queryByField = async (field, since, interval) => {
   const client = new InfluxDB({ url, token })
   let queryClient = client.getQueryApi(org)
   const startDate = since || '1d'
-
+  const every = interval || '1h'
   let fluxQuery = `from(bucket: "terra_temps")
 	|> range(start: -${startDate})
-  |> aggregateWindow(every: 15m, fn: mean)
+  |> aggregateWindow(every: ${every}, fn: mean)
   |> filter(fn: (r) => r["_measurement"] == "mem")
-  |> filter(fn: (r) => r["_field"] == "${field}")`
+  |> filter(fn: (r) => r["_field"] == "${field}")
+  |> limit(n: 100)`
 
   let result = []
   const final = await queryClient
@@ -24,7 +25,7 @@ export const queryByField = async (field, since) => {
         if (row._value !== null) {
           const newObj = {
             time: row._time,
-            value: row._value,
+            value: Number(row._value.toFixed(2)),
             field: row._field
           }
 
